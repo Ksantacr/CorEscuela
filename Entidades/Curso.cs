83e3696ba5d20a7242c6b067cb925ef401a17241
Kevin <defonix0@gmail.com>
2018-11-28 22:17:32 -0500

    objeto curso agregado

--- Entidades/Curso.cs
+++ Entidades/Curso.cs
@@ -1,7 +1,21 @@
+using System;
+
 namespace CoreEscuela.Entidades
 {
+    public enum TiposJornadas {
+        Matutina,
+        Vespertina
+    }
     public class Curso
     {
-        
+        public string UniqueID { get; private set; }
+        public string Nombre { get; set; }
+        public TiposJornadas Jornada { get; set; }
+
+        public Curso(TiposJornadas jornada = TiposJornadas.Matutina) => (UniqueID,  Jornada) = (UniqueID = Guid.NewGuid().ToString(), jornada);
+
+        public override string ToString(){
+            return $"UniqueID: {UniqueID}, Nombre: {Nombre}, Jornada: {Jornada}";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,6 +13,21 @@ namespace CorEscuela
             escuela.TipoEscuela = TipoEscuelas.Primaria;
 
             System.Console.WriteLine(escuela);
+
+            Curso c1 = new Curso{
+                Nombre = "101"
+            };
+            Curso c2 = new Curso{
+                Nombre = "102",
+                Jornada = TiposJornadas.Vespertina
+            };
+            Curso c3 = new Curso{
+                Nombre = "103"
+            };
+
+            System.Console.WriteLine(c1);
+            System.Console.WriteLine(c2);
+            System.Console.WriteLine(c3);
         }
     }
 }
